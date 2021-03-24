e14baeac3989ec51e459e656fe5922767952df32
Frederico Velho <fredericovcoelho@gmail.com>
2021-03-24 00:13:43 -0300

    header-implementation

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <h1>Hello Word</h1>
+      
+    </>
+  )
+}
