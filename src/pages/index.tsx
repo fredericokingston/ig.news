b6be9ab546ca6bd3aceb472cdf06769347d6f3b6
Frederico Velho <fredericovcoelho@gmail.com>
2021-03-25 01:03:33 -0300

    home-implementation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,29 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello Word</h1>
-      
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the<br/>
+          <span>React</span> world</h1>
+          <p>
+            Get acess to all publications<br/>
+            <span>for $9.99</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding"/>        
+      </main>
     </>
   )
 }
