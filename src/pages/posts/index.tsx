2ad9967daadc1cac095095801484da5be1609840
Frederico Velho <fredericovcoelho@gmail.com>
2022-03-27 23:44:14 -0300

    add a render validation on posts page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -26,7 +26,7 @@ export default function Posts({ posts }: PostProps ) {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          { posts.map(post => (
+          { posts && posts.map(post => (
             <Link href={`/posts/${post.slug}`}>
               <a key={post.slug}>
                 <time>{post.updatedAt}</time>
